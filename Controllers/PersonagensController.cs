0edcf16d1b72432a78d54855f23b7ea160704e96
Lschanhi <lschanhi@outlook.com>
2025-09-16 22:25:16 -0300

    utilizando o postman com o banco

--- Controllers/PersonagensController.cs
+++ Controllers/PersonagensController.cs
@@ -1,10 +1,10 @@
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using RpgApi.Models.Emuns;
+using Microsoft.EntityFrameworkCore;
+using RpgApi.Data;
 using RpgApi.Models;
 
 namespace RpgApi.Controllers
@@ -13,69 +13,105 @@ namespace RpgApi.Controllers
     [Route("[controller]")]
     public class PersonagensController : ControllerBase
     {
-        private static List<Personagem> personagens = new List<Personagem>()
-        {
-            //Colar os objetos da lista do chat aqui
-            new Personagem() { Id = 1, Nome = "Frodo", PontosVida=100, Forca=17, Defesa=23, Inteligencia=33, Classe=ClasseEnum.Cavaleiro},
-            new Personagem() { Id = 2, Nome = "Sam", PontosVida=100, Forca=15, Defesa=25, Inteligencia=30, Classe=ClasseEnum.Cavaleiro},
-            new Personagem() { Id = 3, Nome = "Galadriel", PontosVida= 100, Forca=18, Defesa=21, Inteligencia=35, Classe=ClasseEnum.Clerigo },
-            new Personagem() { Id = 4, Nome = "Gandalf", PontosVida=100, Forca=18, Defesa=18, Inteligencia=37, Classe=ClasseEnum.Mago },
-            new Personagem() { Id = 5, Nome = "Hobbit", PontosVida=100, Forca=20, Defesa=17, Inteligencia=31, Classe=ClasseEnum.Cavaleiro },
-            new Personagem() { Id = 6, Nome = "Celeborn", PontosVida=100, Forca=21, Defesa=13, Inteligencia=34, Classe=ClasseEnum.Clerigo },
-            new Personagem() { Id = 7, Nome = "Radagast", PontosVida=100, Forca=25, Defesa=11, Inteligencia=35, Classe=ClasseEnum.Mago },
-            new Personagem() { Id = 8, Nome = "Felipe", PontosVida=1000, Forca=-1000, Defesa=-1000, Inteligencia=-1000, Classe=ClasseEnum.Mendigo }
-        };
-
-        [HttpGet("Get")]
-        public IActionResult GetFirst()
-        {
-            Personagem p = personagens[7];
-            return Ok(p);
-        }
-
-        [HttpGet("GetAll")]
-        public IActionResult Get()
-        {
-            return Ok(personagens);
-        }
-
-        [HttpGet("{Id}")]
-        public IActionResult GetSingle(int id)
-        {
-            return Ok(personagens.FirstOrDefault(pe =>pe.Id == id));
-        }
-        [HttpPost]
-        public IActionResult AddPersonagem(Personagem novoPersonagem)
-        {
-            personagens.Add(novoPersonagem);
-            return Ok(personagens);
-        }
-
-        [HttpPut]
-        public IActionResult UpdatePersonagem (Personagem p)
-        {
-            Personagem personagemAlterado = personagens.Find(pers => pers.Id == p.Id);
-            personagemAlterado.Nome = p.Nome;
-            personagemAlterado.PontosVida = p.PontosVida;
-            personagemAlterado.Forca = p.Forca;
-            personagemAlterado.Defesa = p.Defesa;
-            personagemAlterado.Inteligencia = p.Inteligencia;
-            personagemAlterado.Classe = p.Classe;
-
-            return Ok(personagens);
-        }
         
-        [HttpDelete("{id}")]
-        public IActionResult Delete(int id)
-        {
-            personagens.RemoveAll(pers => pers.Id == id);
+        private readonly DataContext _context;
 
-            return Ok(personagens);
+        public PersonagensController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSingle(int id)
+        {
+            try
+            {
+                Personagem p = await _context.TB_PERSONAGENS.FirstOrDefaultAsync(pBusca => pBusca.Id == id);
+                return Ok (p);
+                
+            }
+            catch (System.Exception ex)
+            {
+                
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet ("GetAll")]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                List<Personagem> lista = await _context.TB_PERSONAGENS.ToListAsync();
+                return Ok(lista);
+                
+            }
+            catch (System.Exception ex)
+            {
+                
+                return BadRequest(ex.Message);
+            }
 
         }
 
-       
+        [HttpPost]
+        public async Task<IActionResult> Add(Personagem novoPersonagem)
+        {
+            try
+            {
+                if(novoPersonagem.PontosVida > 100)
+                {
+                    throw new Exception ("Pontos de Vida Não podem ser Maior que 100");
+                }
+                await _context.TB_PERSONAGENS.AddAsync(novoPersonagem);
+                await _context.SaveChangesAsync();
+                return Ok(novoPersonagem.Id);
+                
+            }
+            catch (System.Exception ex)
+            {
+                
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPut]
+        public async Task<IActionResult> Update(Personagem novoPersonagem)
+        {
+            try
+            {
+                if(novoPersonagem.PontosVida > 100)
+                {
+                    throw new System.Exception("Pontos de Vida Não podem ser maior que 100");
+                }
+                _context.TB_PERSONAGENS.Update(novoPersonagem);
+                int linhasAfetadas = await _context.SaveChangesAsync();
 
+                return Ok(linhasAfetadas);
+            }
+            catch (System.Exception ex)
+            {
+                
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                Personagem pRemove = await _context.TB_PERSONAGENS.FirstOrDefaultAsync(p => p.Id == id);
+                _context.TB_PERSONAGENS.Remove(pRemove);
+                int linhasAfetadas = await _context.SaveChangesAsync();
+
+                return Ok(linhasAfetadas);
+            }
+            catch (System.Exception ex)
+            {
+                
+               return BadRequest(ex.Message);
+            }
+        }
 
 
 
